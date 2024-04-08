8530f9d955567f41f5b135091bb5108c8cc0381b
Guess <Guess56@yandex.ru>
2024-04-08 22:09:05 +0500

    Третий коммит заменил вывод Toast по нажатию кнопки на код переходов на реальные экраны.

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,29 +1,32 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import android.widget.Toast
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val buttomSearch = findViewById<Button>(R.id.search)
-        buttomSearch.setOnClickListener {
-            Toast.makeText(this@MainActivity, "Нажали на кнопку Поиск", Toast.LENGTH_SHORT).show()
+
+        val optionButton = findViewById<Button>(R.id.option)
+        optionButton.setOnClickListener{
+            val optionIntetnt = Intent(this,SettingsActivity::class.java)
+            startActivity(optionIntetnt)
         }
-        val buttomMedia = findViewById<Button>(R.id.media)
-        val buttomClickListener: View.OnClickListener = object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                Toast.makeText(this@MainActivity, "Нажали на кнопку Медиатека", Toast.LENGTH_SHORT).show()
-            }
+        val searchButton = findViewById<Button>(R.id.search)
+        searchButton.setOnClickListener{
+            val searchIntetnt = Intent(this, SearchActivity::class.java)
+            startActivity(searchIntetnt)
         }
-        buttomMedia.setOnClickListener(buttomClickListener)
-        val buttomOption = findViewById<Button>(R.id.option)
-        buttomOption.setOnClickListener {
-            Toast.makeText(this@MainActivity, "Нажали на кнопку Настройки", Toast.LENGTH_SHORT).show()
+        val mediaButton = findViewById<Button>(R.id.media)
+        mediaButton.setOnClickListener{
+            val mediaIntetnt = Intent(this, MediaActivity::class.java)
+            startActivity(mediaIntetnt)
         }
+
     }
 }
