e23223e5c9cd7893603ccba4044fc143c83a45f6
Guess <Guess56@yandex.ru>
2024-04-20 14:02:23 +0500

    Cедьмой коммит, реализован activitySearch

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,11 +1,50 @@
 package com.example.playlistmaker
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.isVisible
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+        val backButton = findViewById<Toolbar>(R.id.toolbarSearch)
+
+        backButton.setOnClickListener{
+            onBackPressedDispatcher.onBackPressed()
+        }
+
+        val inputEditText = findViewById<EditText>(R.id.editTextSearch)
+        val clearButton = findViewById<ImageView>(R.id.clearIcon)
+        clearButton.setOnClickListener {
+            inputEditText.setText("")
+        }
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s.isNullOrEmpty()) {
+                    clearButton.isVisible = false
+                } else {
+                    clearButton.isVisible = true
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // empty
+            }
+        }
+        inputEditText.addTextChangedListener(simpleTextWatcher)
     }
+
 }
