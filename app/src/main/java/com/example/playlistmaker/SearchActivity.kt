64f5bd30f6a0d6e38e64f3b3cf1491f71bcff146
Guess <Guess56@mail.ru>
2024-05-19 22:39:34 +0500

    Четырнадцатый коммит Добавленны поисковые запросы при помощи Api

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,42 +1,78 @@
 package com.example.playlistmaker
 
-
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.isVisible
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 
 class SearchActivity : AppCompatActivity() {
+
+
+    private val itunesBaseUrl = "https://itunes.apple.com"
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(itunesBaseUrl)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+    private val itunesAPI = retrofit.create(itunesAPI::class.java)
+
     private companion object {
         const val PRODUCT_AMOUNT = "PRODUCT_AMOUNT"
         const val AMOUNT_DEF = ""
     }
     private var editValue: String = AMOUNT_DEF
-    private lateinit var inputEditText:EditText
+    private lateinit var inputEditText: EditText
+    private lateinit var imageError: ImageView
+    private lateinit var placeholderMessage: TextView
+    private lateinit var refresh: Button
+    val track = ArrayList<Track>()
+    val adapter = TrackAdapter(track)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
         val backButton = findViewById<Toolbar>(R.id.toolbarSearch)
+        imageError = findViewById(R.id.iv_Error)
+        placeholderMessage = findViewById(R.id.tv_Error)
+        refresh = findViewById(R.id.Refresh)
 
-        backButton.setOnClickListener{
+
+
+
+
+        backButton.setOnClickListener {
             onBackPressedDispatcher.onBackPressed()
         }
 
-
-
-        inputEditText = findViewById<EditText>(R.id.editTextSearch)
+        inputEditText = findViewById(R.id.editTextSearch)
         val clearButton = findViewById<ImageView>(R.id.clearIcon)
+
         clearButton.setOnClickListener {
             inputEditText.setText("")
-        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
-        inputMethodManager?.hideSoftInputFromWindow(`inputEditText`.windowToken, 0)
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            inputMethodManager?.hideSoftInputFromWindow(`inputEditText`.windowToken, 0)
+            track.clear()
+            adapter.notifyDataSetChanged()
         }
 
         val simpleTextWatcher = object : TextWatcher {
@@ -52,18 +88,23 @@ class SearchActivity : AppCompatActivity() {
                 // empty
             }
         }
+        inputEditText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                // ВЫПОЛНЯЙТЕ ПОИСКОВЫЙ ЗАПРОС ЗДЕСЬ
+                search()
+                true
+            }
+            false
+        }
         inputEditText.addTextChangedListener(simpleTextWatcher)
-        val trackAdapter = TrackAdapter(
-            listOf(
-                Track("Smells Like Teen Spirit", "Nirvana", "5:01","https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"),
-                Track("Billie Jean", "Michael Jackson", "4:35","https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"),
-                Track("Stayin' Alive", "Bee Gees", "4:10","https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"),
-                Track("Whole Lotta Love", "Led Zeppelin", "5:33","https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"),
-                Track("Sweet Child O'Mine", "Guns N' Roses", "5:03","https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg")
-            )
-        )
+
+        refresh.setOnClickListener{
+            search()
+        }
+
         val rvTrack = findViewById<RecyclerView>(R.id.rvTrack)
-        rvTrack.adapter = trackAdapter
+        rvTrack.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        rvTrack.adapter = adapter
 
     }
     override fun onSaveInstanceState(outState: Bundle) {
@@ -76,5 +117,57 @@ class SearchActivity : AppCompatActivity() {
         editValue = savedInstanceState.getString(PRODUCT_AMOUNT, AMOUNT_DEF)
         inputEditText.setText(editValue)
     }
+    private fun showMessage(text: String, additionalMessage: String) {
+        if (text.isNotEmpty()) {
+            placeholderMessage.visibility = View.VISIBLE
+            track.clear()
+            adapter.notifyDataSetChanged()
+            placeholderMessage.text = text
+            if (additionalMessage.isNotEmpty()) {
+                Toast.makeText(applicationContext, additionalMessage, Toast.LENGTH_LONG)
+                    .show()
+            }
+        } else {
+            placeholderMessage.visibility = View.GONE
+        }
+    }
+    private fun search() {
+        if (inputEditText.text.isNotEmpty()) {
+            itunesAPI.search(inputEditText.text.toString()).enqueue(object :
+                Callback<TrackResponse> {
+                override fun onResponse(call: Call<TrackResponse>, response: Response<TrackResponse>
+                ){
+                    if (response.code() == 200) {
+                        track.clear()
+                        if (response.body()?.results?.isNotEmpty() == true) {
+                            track.addAll(response.body()?.results!!)
+                            adapter.notifyDataSetChanged()
+                        }
+                        if (track.isEmpty()) {
+                            showMessage(getString(R.string.nothing_found), "")
+                            imageError.visibility = View.VISIBLE
+                            imageError.setImageResource(R.drawable.nothing_found)
+                        } else {
+                            showMessage("", "")
+                        }
+                    } else {
+                        showMessage(
+                            getString(R.string.something_went_wrong),
+                            response.code().toString()
+                        )
+                    }
+                }
 
+                override fun onFailure(call: Call<TrackResponse>, t: Throwable) {
+                    showMessage(getString(R.string.something_went_wrong), t.message.toString())
+                    imageError.visibility = View.VISIBLE
+                    imageError.setImageResource(R.drawable.connect)
+                    refresh.visibility = View.VISIBLE
+                }
+
+            })
+        }
+
+    }
 }
+
